37b824f77084ec0b15bd553dbfca2e3348bb6e41
Sona Mikayelyan <sonamikayelyan2006.com>
2026-09-20 23:14:51 +0400

    adding bubble and selection sort

---
+++ Algorithms/Sorting/bubbleSort.js
@@ -0,0 +1,35 @@
+function bubbleSort (arr){
+
+    for(let i = 0; i < arr.length; i++){
+
+        let swapCount = 0;
+
+        for(let j = 0; j < arr.length - 1 - i; j++){
+
+            if(arr[j] > arr[j+1]){
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+                swapCount++;
+            }
+        }
+
+        if(swapCount === 0){
+            return arr;
+        }
+    }
+
+    return arr;
+}
+
+//test case
+console.log(bubbleSort([14,4,3,2,84,1,6]));
+
+/* 
+Time Complexity
+
+Worst Case: O(n^2)
+Best Case: O(n)
+Average Case: O(n^2)
+
+Space Complexity
+O(1)
+*/
---
+++ Algorithms/Sorting/selectionSort.js
@@ -0,0 +1,34 @@
+function selectionSort(arr){
+
+    for(let i = 0; i < arr.length - 1; i++){
+
+        let minIndex = i;
+
+        for(let j = i + 1; j < arr.length ;j++){
+
+            if(arr[minIndex] > arr[j]){
+                minIndex = j;
+            }
+        }
+        if(minIndex !== i){
+            [arr[i],arr[minIndex]] = [arr[minIndex],arr[i]];
+        }
+    }
+
+    return arr;
+}
+
+//test case
+console.log(selectionSort([14,4,3,2,84,1,6]));
+
+
+/*
+Time Complexity
+
+Worst Case: O(n^2)
+Best Case: O(n^2)
+Averge Case: O(n^2)
+
+Space Complexity
+O(1)
+*/
