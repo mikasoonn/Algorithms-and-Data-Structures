d32b3b953453cd0f43fac361a98b2cd46d1638e3
Sona Mikayelyan <sonamikayelyan2006.com>
2026-09-22 22:16:37 +0400

    adding insertionSort

---
+++ Algorithms/Sorting/insertionSort.js
@@ -0,0 +1,27 @@
+function insertionSort(arr){
+    for(let i = 1; i < arr.length; i++){
+        let key = arr[i];
+        let j = i - 1;
+        while(j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+    return arr;
+}
+
+let arr = [8,5,1,4,3,2,9,1.5];
+
+//test case
+console.log(insertionSort(arr));
+
+//Time Complexity
+
+//Worst Case: O(n^2)
+//Best Case: O(n)
+//Average Case O(n^2)
+
+//Space Complexity
+
+// O(1);
